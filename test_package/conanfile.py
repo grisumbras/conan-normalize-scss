b982ce2acc8dd1a1949520a46671d713a393bdb4
Dmitry Arkhipov <grisumbras@gmail.com>
2019-07-15 12:29:28 +0300

    install sassc for test

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,9 +1,20 @@
-from conans import ConanFile
+from conans import (
+    ConanFile,
+    tools,
+)
 import os
 
 
 class TestNormalizeCss(ConanFile):
     settings = "os_build"
+
+    def system_requirements(self):
+        sassc = "sassc"
+        installer = tools.SystemPackageTool()
+        installer.install(sassc)
+
+    def build(self):
+        pass
 
     def test(self):
         if self.settings.os_build == "Windows":
